9cd9922191dca0f95f66c4694fec55a45cf96a54
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-06-15 22:40:28 -0500

    finish later

---
+++ src/FlynnKunz/StaffMenuUI/forms/subforms/VanishForm.php
@@ -0,0 +1,39 @@
+<?php 
+
+/* Copyright 2022-2022 FlynnKunz and fernanACM
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+namespace FlynnKunz\StaffMenuUI\forms\subforms;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\player\GameMode;
+
+use Vecnavium\FormsUI\SimpleForm;
+
+use FlynnKunz\StaffMenuUI\Main;
+use FlynnKunz\StaffMenuUI\utils\PluginUtils;
+
+class VanishForm{
+
+    public function VanishForm(Player $player){
+        $form = new SimpleForm(function(Player $player, $data){
+            if($data !== null){
+                switch($data){
+                    case 0:
+                    break;
+                   
+                }
+            }
+        });
+    }
+}
